369348bd739a99df025c508642ed606b9f48f934
arukas2580 <jorugi00@gmail.com>
2016-12-07 23:51:20 +0900

    20161207_다시 배포

--- jorugiBoard2/src/main/java/com/example/jorugiBoard/controller/BoardController.java
+++ jorugiBoard2/src/main/java/com/example/jorugiBoard/controller/BoardController.java
@@ -2,6 +2,8 @@ package com.example.jorugiBoard.controller;
 
 import java.util.List;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,7 @@ import com.example.jorugiBoard.service.JorugiService;
 public class BoardController {
 	
 	@Autowired
+	@Resource(name="JorugiService")
 	private JorugiService jorugiService;
 
 
